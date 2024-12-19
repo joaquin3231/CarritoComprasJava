56c0274adb06932c363c86911f646090d33f8230
Pintos Joaquin <joale7546@gmail.com>
2024-12-19 10:16:02 -0300

    edicion de URL

--- src/main/java/com/proyecto/joaquin/controllers/ProjectControllers.java
+++ src/main/java/com/proyecto/joaquin/controllers/ProjectControllers.java
@@ -17,6 +17,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.multipart.MultipartFile;
@@ -36,6 +37,7 @@ import jakarta.validation.Valid;
 
 
 @Controller
+@RequestMapping("https://carritocomprasjava.onrender.com")
 public class ProjectControllers {
 	@Autowired
 	private UserServ userServ;
